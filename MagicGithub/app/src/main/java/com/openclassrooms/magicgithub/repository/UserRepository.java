eba6c8bcb3f84a49d104d3db1fc7a04a823c9da5
Guillaume <gcudel@gmail.com>
2019-12-17 16:34:57 +0100

    Import de MagicGitHub p2 terminé

---
+++ MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -0,0 +1,38 @@
+package com.openclassrooms.magicgithub.repository;
+
+import com.openclassrooms.magicgithub.api.ApiService;
+import com.openclassrooms.magicgithub.api.FakeApiServiceGenerator;
+import com.openclassrooms.magicgithub.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS_RANDOM;
+
+public class UserRepository {
+
+    private final ApiService apiService; // TODO: A utiliser
+
+    public UserRepository(ApiService apiService) {
+
+        this.apiService = apiService;
+    }
+
+    public List<User> getUsers() {
+        // TODO: A modifier
+
+        return apiService.getUsers();
+    }
+
+    public void generateRandomUser() {
+        // TODO: A modifier
+        apiService.generateRandomUser();
+    }
+
+    public void deleteUser(User user) {
+
+        // TODO: A modifier
+        apiService.deleteUser(user);
+    }
+}
